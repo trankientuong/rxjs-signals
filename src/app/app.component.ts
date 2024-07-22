6f3aa91c15e8b3dce45a477c48d82e3e0fb16d5c
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-22 07:32:06 +0700

    Converting Observables To Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+  $interval = interval(1000);
+  intervalSignal = toSignal(this.$interval, { initialValue: 0 });
   // interval = signal(0);
   // doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
@@ -26,11 +28,11 @@ export class AppComponent implements OnInit {
     //   this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1);
     //   // update some signal
     // }, 1000);
-    
+
     // const subscription = interval(1000).pipe(
     //   map((val) => val * 2)
     // ).subscribe({
-    //   next: (val) => console.log(val) 
+    //   next: (val) => console.log(val)
     // });
 
     // this.destroyRef.onDestroy(() => {
@@ -38,7 +40,7 @@ export class AppComponent implements OnInit {
     // });
 
     const subscription = this.clickCount$.subscribe({
-      next: (val) => console.log(`Clicked button ${val} times.`)
+      next: (val) => console.log(`Clicked button ${val} times.`),
     });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
